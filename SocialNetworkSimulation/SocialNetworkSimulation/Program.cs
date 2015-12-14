dbdfd5d7a9970556eaa2bc275b0baa0eacd0a2fc
Lucas <lucaswyland11@gmail.com>
2015-12-13 19:24:54 -0600

    Unweighted Closeness Centrality

--- SocialNetworkSimulation/SocialNetworkSimulation/Program.cs
+++ SocialNetworkSimulation/SocialNetworkSimulation/Program.cs
@@ -9,6 +9,7 @@ using System.IO;
 using CsvHelper;
 using System.Diagnostics.Contracts;
 using System.Diagnostics;
+using QuickGraph.Collections;
 
 namespace SocialNetworkSimulation
 {
@@ -23,13 +24,24 @@ namespace SocialNetworkSimulation
 
             // Project Input
             var graph = ImportGraphFromFile();
-
             var undirectedGraph = ImportUndirectedGraphFromFile();
 
             // Calculate Weighted/Unweighted In-Out Degrees
             calculateInOutDegrees(graph);
 
+            // Unweighted Closeness Centrality Calculation Directed
+            calculateClosenessCentrality(graph);
+
+            // Unweighted Closeness Centrality Calculation Undirected
+            calculateClosenessCentrality(undirectedGraph);
+
+
+
+
+
         }
+
+
 
         static BidirectionalGraph<string, Edge<string>> ImportGraphFromFile()
         {
@@ -197,6 +209,105 @@ namespace SocialNetworkSimulation
                     numberOfVerticesByWeightedOutDegree.Add(edgeSum, 1);
                 }
             }
+        }
+
+        static void calculateClosenessCentrality(BidirectionalGraph<string, Edge<string>> graph)
+        {
+            var averageDistanceByStartingVertex = new Dictionary<string, int>();
+            var distancesByVertex = new Dictionary<string, int>();
+
+            foreach (var v in graph.Vertices)
+            {
+                // Calculate Paths
+                distancesByVertex = dijkstras(graph, v);
+
+                // Remove Infinity
+                var distances = distancesByVertex.Values.ToList();
+                distances.RemoveAll(d => d == int.MaxValue);
+
+                // Calculate Average
+                var average = 0;
+                foreach (var d in distances)
+                    average += d;
+
+                average = average / distances.Count;
+
+                // Add To Result Dictionary
+                averageDistanceByStartingVertex.Add(v, average);
+            }
+        }
+
+
+        public static Dictionary<string, int> dijkstras(BidirectionalGraph<string, Edge<string>> g, string startVertex)
+        {
+            // Gives a enumerable object of the adjacent edges to startVertex
+            IEnumerable<Edge<string>> adjEdges = g.OutEdges(startVertex);
+
+            //			// Dictionary to hold the distances from startVertex to all verticies.
+            //			Dictionary<string,int> distances = new Dictionary<string,int> ();
+            ////			foreach (var v in g.Vertices)
+            ////				distances.Add (v, -1);
+            //
+            //
+            //			// Dictionary to store the previous node in shortest path.
+            //			Dictionary<string,string> prevNode = new Dictionary<string,string>();
+            ////			foreach (var v in g.Vertices)
+            ////				prevNode.Add (v, null);
+
+            // FibonacciQueue to be used in place of a Priority Queue.
+            // Stores the unvisted nodes during runtime.
+            // var unvisitedNodes = new FibonacciQueue<string,int>(distances);
+
+            var unvistedNodes = new FibonacciHeap<int, string>();
+            Dictionary<string, int> distances = new Dictionary<string, int>();
+
+
+            // The distance from the start to itself is zero.
+            if (!distances.ContainsKey(startVertex))
+                distances.Add(startVertex, 0);
+
+            foreach (var v in g.Vertices)
+            {
+                if (v != startVertex)
+                {
+                    if (!distances.ContainsKey(v))
+                    {
+                        distances.Add(v, int.MaxValue);
+                    }
+
+                    //unvistedNodes.Enqueue (distances [v], v);
+                }
+            }
+            unvistedNodes.Enqueue(distances[startVertex], startVertex);
+
+            //Console.WriteLine (unvistedNodes);
+
+
+            while (!unvistedNodes.IsEmpty)
+            {
+                string u = unvistedNodes.Top.Value;
+                unvistedNodes.Dequeue();
+
+                foreach (var e in g.OutEdges(u))
+                {
+                    //					int edgeWeight = findEdgeWeight (g, u, v)
+                    //Console.WriteLine(e.Target);
+                    var alt = distances[u] + e.weight;
+                    if (alt < distances[e.Target])
+                    {
+                        distances[e.Target] = alt;
+                        //var tmp = new KeyValuePair<int,string>;
+
+                        unvistedNodes.Enqueue(alt, e.Target);
+                        //unvistedNodes.ChangeKey (unvistedNodes., alt);
+                        //						unvistedNodes.ElementAt(
+                        int temp = distances[e.Target];
+                        //Console.WriteLine (e);
+                    }
+                }
+            }
+
+            return distances;
         }
 
         /** Custom Data Structure Helper Classes **/
